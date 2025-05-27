33ec5f29dcb82fbaf136c87ca2b2bb0ff2eaa0aa
me-safwan-07 <mesafwan07@gmail.com>
2025-05-27 23:41:10 +0530

    feat: update dependencies for Radix UI label and React Colorful, and adjust Tailwind config path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/**/*.{ts,tsx}",
+    // "../../packages/ui/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
