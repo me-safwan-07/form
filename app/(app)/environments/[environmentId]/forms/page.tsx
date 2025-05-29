287323d49e2aa73e937f2fe15c8b398ae77bdcdf
me-safwan-07 <mesafwan07@gmail.com>
2025-05-29 23:55:03 +0530

    feat: enhance Forms page with CreateSurveyButton and PageHeader integration

--- app/(app)/environments/[environmentId]/forms/page.tsx
+++ app/(app)/environments/[environmentId]/forms/page.tsx
@@ -1,11 +1,14 @@
 import { authOptions } from "@/packages/lib/authOptions";
-import { WEBAPP_URL } from "@/packages/lib/constants";
+import { FORMS_PER_PAGE, SURVEYS_PER_PAGE, WEBAPP_URL } from "@/packages/lib/constants";
 import { getEnvironment } from "@/packages/lib/environment/service";
 import { getFormCount } from "@/packages/lib/form/service";
 import { getProductByEnvironmentId } from "@/packages/lib/product/service";
+import { Button } from "@/packages/ui/Button";
 // import { Button } from "@/packages/ui/Button"
 import { PageContentWrapper } from "@/packages/ui/PageContentWrapper";
+import { PageHeader } from "@/packages/ui/PageHeader";
 import { TemplateList } from "@/packages/ui/TemplateList";
+import { PlusIcon } from "lucide-react";
 import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 
@@ -39,13 +42,28 @@ const Page = async ({ params }: FormTemplateProps) => {
 
     const formCount = await getFormCount(params.environmentId);
 
-    console.log(formCount)
+    const CreateSurveyButton = (
+        <Button
+            size="sm"
+            href={`/environments/${environment.id}/forms/template`}
+            variant="darkCTA"
+            EndIcon={PlusIcon}>
+            New survey
+        </Button>
+    );
+
 
     return (
         <PageContentWrapper>
             {formCount > 0 ? (
                 <>
-                    
+                    <PageHeader pageTitle="Forms" cta={CreateSurveyButton} />
+                    <FormsList 
+                        environment={environment}
+                        WEBAPP_URL={WEBAPP_URL}
+                        userId={session.user.id}
+                        formsPerPage={FORMS_PER_PAGE}
+                    />
                 </>
             ) : (
                 <>
